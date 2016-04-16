b05c83682dcf9ae8354750d957ee9219f198b141
Teng Xu <xut@uber.com>
2016-04-15 22:37:00 -0700

    Populate TaskInfo with DiscoveryInfo for CassandraDaemon Task

--- cassandra-commons/src/main/java/com/mesosphere/dcos/cassandra/common/tasks/CassandraDaemonTask.java
+++ cassandra-commons/src/main/java/com/mesosphere/dcos/cassandra/common/tasks/CassandraDaemonTask.java
@@ -358,6 +358,9 @@ public class CassandraDaemonTask extends CassandraTask {
     }
 
     @Override
+    public int getNativeTransportPort() { return config.getApplication().getNativeTransportPort(); }
+
+    @Override
     public List<Resource> getReserveResources() {
         return Arrays.asList(
 
@@ -396,4 +399,28 @@ public class CassandraDaemonTask extends CassandraTask {
     public int hashCode() {
         return Objects.hash(config);
     }
+
+
+    @Override
+    public Protos.TaskInfo toProto() {
+        Protos.DiscoveryInfo.Builder discovery = Protos.DiscoveryInfo.newBuilder();
+        Protos.Ports.Builder discoveryPorts = Protos.Ports.newBuilder();
+        discoveryPorts.addPorts(0, Protos.Port.newBuilder().setNumber(getNativeTransportPort()).setName("NativeTransport"));
+        discovery.setPorts(discoveryPorts);
+        discovery.setVisibility(Protos.DiscoveryInfo.Visibility.EXTERNAL);
+        discovery.setName(config.getApplication().getClusterName() + "." + name );
+
+        return Protos.TaskInfo.newBuilder()
+                .setTaskId(Protos.TaskID.newBuilder().setValue(id))
+                .setSlaveId(Protos.SlaveID.newBuilder().setValue(slaveId))
+                .setName(name)
+                .setData(getTaskData().toByteString())
+                .addAllResources(getReserveResources())
+                .addAllResources(getCreateResources())
+                .addAllResources(getLaunchResources())
+                .setExecutor(executor.toExecutorInfo(role, principal))
+                .setDiscovery(discovery.build())
+                .build();
+    }
+
 }
