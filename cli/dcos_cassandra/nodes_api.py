91f33293f59795a704fa0cf7d6fe8c1177e7289b
Nick <nick@mesosphere.io>
2016-04-19 11:35:32 -0700

    Verbatim copy of dcos-cassandra-cli:master into the framework repo
    (snapshot of dcos-cassandra-cli state as of rev 722f62d2)
    
    Avoid juggling two different repos, and make it easier to keep changes
    in sync. CI could even integrate the current CLI module in build/test
    runs!

---
+++ cli/dcos_cassandra/nodes_api.py
@@ -0,0 +1,56 @@
+#
+#    Copyright (C) 2015 Mesosphere, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
+from dcos_cassandra import cassandra_utils as cu
+from dcos import http
+
+
+def list():
+    return cu.to_json(http.get(cu.api_url("/nodes/list"), headers={}))
+
+
+def describe(id):
+    return cu.to_json(http.get(cu.api_url("/nodes/node-{}/info".format(str(id))), headers={}))
+
+
+def status(id):
+    return cu.to_json(http.get(cu.api_url("/nodes/node-{}/status".format(str(id))), headers={}))
+
+
+def restart(id):
+    response = http.put(cu.api_url("/nodes/restart?node=node-{}".format(str(id))), headers={})
+    return response.status_code % 200 < 100
+
+
+def replace(id):
+    response = http.put(cu.api_url("/nodes/replace?node=node-{}".format(str(
+            id))), headers={})
+    return response.status_code % 200 < 100
+
+
+def connection():
+    return cu.to_json(http.get(cu.api_url("/nodes/connect"), headers={}))
+
+
+def connection_address():
+    return cu.to_json(http.get(cu.api_url("/nodes/connect/address"),
+                               headers={}))
+
+
+def connection_dns():
+    return cu.to_json(http.get(cu.api_url("/nodes/connect/dns"),
+                               headers={}))
