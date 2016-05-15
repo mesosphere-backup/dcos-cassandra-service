5405a587dec8b0fea8f27b78563e96f1e304f704
Ken Sipe <kensipe@gmail.com>
2016-05-15 15:46:34 -0500

    cli versions will not be managed this way

--- cli/dcos_cassandra/constants.py
+++ cli/dcos_cassandra/constants.py
@@ -12,5 +12,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-version = '1.0.5'
+version = '0.1.0'
 """DCOS Cassandra version"""
