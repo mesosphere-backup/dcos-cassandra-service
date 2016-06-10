c50c292e6ea10686e6c3d86bb840a97796411213
Ken Sipe <kensipe@gmail.com>
2016-06-10 10:58:38 -0500

    builds linux and darwin if on darwin. builds the sha file

--- cli/bin/binary.sh
+++ cli/bin/binary.sh
@@ -17,12 +17,43 @@
 echo "Building binary..."
 pyinstaller binary/binary.spec
 
+docker-check() {
+  time=2
+  command="/bin/sh -c \"docker ps\""
+
+  if hash expect 2>/dev/null; then
+    expect -c "set echo \"-noecho\"; set timeout $time; spawn -noecho $command; expect timeout { exit 1 } eof { exit 0 }"
+
+    if [ $? = 1 ] ; then
+      echo "Docker execution timed out. Make sure docker-machine start docker-vm is started."
+      exit 0;
+    fi
+   fi
+}
+
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform
     mkdir -p dist/darwin
     mv dist/dcos-cassandra dist/darwin
-    echo "Darin Build... building linux"
+    shasum -a 256 dist/darwin/dcos-cassandra | awk '{print $1}' > dist/darwin/dcos-cassandra.sha1
+    echo "Darin Build Complete!"
 
+    # linux build on a darwin plaform if docker runs
+    docker-check
+    docker rmi -f cassandra-binary || true
+    docker rm cassandra-binary || true
+    docker build -f binary/Dockerfile.linux-binary -t cassandra-binary .
+    docker run --name cassandra-binary cassandra-binary
+    mkdir -p dist/linux
+    docker cp cassandra-binary:/dcos-cassandra/dist/linux dist/
+    shasum -a 256 dist/linux/dcos-cassandra | awk '{print $1}' > dist/linux/dcos-cassandra.sha1
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+      # Do something under GNU/Linux platform  #statements
+      mkdir -p dist/linux
+      mv dist/dcos-cassandra dist/linux
+      sha256sum dist/linux/dcos-cassandra | awk '{print $1}' > dist/linux/dcos-cassandra.sha1
+      echo "Linux Build Complete"
 fi
 
 echo "Build finished!"
