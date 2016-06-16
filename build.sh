ce878e48a99692d8c9ed92811db7ba4d93f4853b
Michael Browning <mbrowning@mesosphere.com>
2016-06-16 14:00:26 -0700

    Move some environment massaging out of build.sh

--- build.sh
+++ build.sh
@@ -2,21 +2,33 @@
 
 # In theory, we could use Jenkins' "Multi SCM" script, but good luck with getting auto-build to work with that
 # Instead, clone the secondary 'dcos-tests' repo manually.
-git clone --depth 1 git@github.com:mesosphere/dcos-tests.git
+if [ ! -d dcos-tests ]; then
+    git clone --depth 1 git@github.com:mesosphere/dcos-tests.git
+fi
 echo Running with dcos-tests rev: $(git --git-dir=dcos-tests/.git rev-parse HEAD)
 
 # GitHub notifier config
-export _GITHUB_SCRIPT="dcos-tests/build/update-github-status.sh"
-export GITHUB_COMMIT_STATUS_URL="http://velocity.mesosphere.com/service/velocity/job/${JOB_NAME}/${BUILD_ID}/console"
-export GIT_REPOSITORY_ROOT=$(pwd)
+
+if [ -n "$JENKINS_HOME" ]; then
+    # we're in a CI build. send outcomes to github.
+    # note: we expect upstream to specify GITHUB_COMMIT_STATUS_URL and GIT_REPOSITORY_ROOT in this case
+    _notify_github() {
+        ./dcos-tests/build/update-github-status.sh $1 $2 $3
+    }
+else
+    # we're being run manually. print outcomes.
+    _notify_github() {
+        echo "[STATUS:build.sh] $2 $1: $3"
+    }
+fi
 
 # Build steps for Cassandra
 
-$_GITHUB_SCRIPT pending build "Build running"
+_notify_github pending build "Build running"
 
 ./gradlew clean distZip
 if [ $? -ne 0 ]; then
-  $_GITHUB_SCRIPT failure build "Gradle build failed"
+  _notify_github failure build "Gradle build failed"
   exit 1
 fi
 
@@ -24,20 +36,17 @@ fi
 sed -i 's/parallel=true/parallel=false/g' gradle.properties
 ./gradlew check
 if [ $? -ne 0 ]; then
-  $_GITHUB_SCRIPT failure build "Unit tests failed"
+  _notify_github failure build "Unit tests failed"
   exit 1
 fi
 
-# hack: cli tests want EXACTLY python 3.4, while alpine is on 3.5
-ln -s /usr/bin/python3 /usr/bin/python3.4
-which python3.4
 make --directory=cli/ all
 if [ $? -ne 0 ]; then
-  $_GITHUB_SCRIPT error build "CLI build/tests failed"
+  _notify_github error build "CLI build/tests failed"
   exit 1
 fi
 
-$_GITHUB_SCRIPT success build "Build succeeded"
+_notify_github success build "Build succeeded"
 
 # No more github updates from here onwards:
 # ci-test.sh and ci-upload.sh helpers both handle this internally
