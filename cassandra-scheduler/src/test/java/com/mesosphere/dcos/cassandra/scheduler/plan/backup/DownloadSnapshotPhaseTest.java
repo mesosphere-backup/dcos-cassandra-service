1035dbd2aee7028e9f2e35f9335a57c8e4e78236
Mohit Soni <mohitsoni1989@gmail.com>
2016-06-23 13:56:45 -0700

    Added more tests

--- cassandra-scheduler/src/test/java/com/mesosphere/dcos/cassandra/scheduler/plan/backup/DownloadSnapshotPhaseTest.java
+++ cassandra-scheduler/src/test/java/com/mesosphere/dcos/cassandra/scheduler/plan/backup/DownloadSnapshotPhaseTest.java
@@ -1,12 +1,11 @@
 package com.mesosphere.dcos.cassandra.scheduler.plan.backup;
 
 import com.mesosphere.dcos.cassandra.common.tasks.CassandraDaemonTask;
-import com.mesosphere.dcos.cassandra.common.tasks.backup.BackupContext;
+import com.mesosphere.dcos.cassandra.common.tasks.backup.RestoreContext;
 import com.mesosphere.dcos.cassandra.scheduler.offer.ClusterTaskOfferRequirementProvider;
 import com.mesosphere.dcos.cassandra.scheduler.tasks.CassandraTasks;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.MapUtils;
-import org.apache.mesos.scheduler.plan.Block;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,13 +16,10 @@ import org.mockito.MockitoAnnotations;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.mockito.Mockito.when;
 
-public class BackupSnapshotPhaseTest {
-    public static final String SNAPSHOT_NODE_0 = "snapshot-node-0";
-    public static final String NODE_0 = "node-0";
+public class DownloadSnapshotPhaseTest {
     @Mock
     private ClusterTaskOfferRequirementProvider provider;
     @Mock
@@ -36,39 +32,32 @@ public class BackupSnapshotPhaseTest {
 
     @Test
     public void testCreateBlocksEmpty() {
-        final BackupContext context =  BackupContext.create("", "", "", "", "", "");
+        final RestoreContext context =  RestoreContext.create("", "", "", "", "", "");
 
         when(cassandraTasks.getDaemons()).thenReturn(MapUtils.EMPTY_MAP);
-        final BackupSnapshotPhase phase = new BackupSnapshotPhase(context, cassandraTasks, provider);
-        final List<BackupSnapshotBlock> blocks = phase.createBlocks();
+        final DownloadSnapshotPhase phase = new DownloadSnapshotPhase(context, cassandraTasks, provider);
+        final List<DownloadSnapshotBlock> blocks = phase.createBlocks();
 
         Assert.assertNotNull(blocks);
         Assert.assertTrue(CollectionUtils.isEmpty(blocks));
-        Assert.assertEquals("Snapshot", phase.getName());
+        Assert.assertEquals("Download", phase.getName());
     }
 
     @Test
     public void testCreateBlocksSingle() {
-        final BackupContext context =  BackupContext.create("", "", "", "", "", "");
+        final RestoreContext context =  RestoreContext.create("", "", "", "", "", "");
 
         final CassandraDaemonTask daemonTask = Mockito.mock(CassandraDaemonTask.class);
         final HashMap<String, CassandraDaemonTask> map = new HashMap<>();
-        map.put(NODE_0, daemonTask);
+        map.put("node-0", daemonTask);
         when(cassandraTasks.getDaemons()).thenReturn(map);
-        when(cassandraTasks.get(SNAPSHOT_NODE_0)).thenReturn(Optional.of(daemonTask));
-        final BackupSnapshotPhase phase = new BackupSnapshotPhase(context, cassandraTasks, provider);
-        final List<? extends Block> blocks = phase.getBlocks();
+        when(cassandraTasks.get("download-node-0")).thenReturn(Optional.of(daemonTask));
+        final DownloadSnapshotPhase phase = new DownloadSnapshotPhase(context, cassandraTasks, provider);
+        final List<DownloadSnapshotBlock> blocks = phase.createBlocks();
 
         Assert.assertNotNull(blocks);
         Assert.assertTrue(blocks.size() == 1);
 
-        final Block block = blocks.get(0);
-        Assert.assertTrue(block instanceof BackupSnapshotBlock);
-        Assert.assertEquals("snapshot-node-0", block.getName());
-
-        final UUID blockId = block.getId();
-
-        final UUID getId = UUID.fromString(blockId.toString());
-        Assert.assertEquals(block, phase.getBlock(getId));
+        Assert.assertEquals("download-node-0", blocks.get(0).getName());
     }
 }
