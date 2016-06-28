4b753653ac7f66de4e318c8614b9ddd007604441
Mohit Soni <mohitsoni1989@gmail.com>
2016-06-28 14:53:16 -0700

    Added publish.sh

--- cli/bin/publish.sh
+++ cli/bin/publish.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # env vars
 #   PLATFORM (e.g. linux-x86-64)
 
-set -eux -o pipefail
+set -eux
 
 export VERSION="${GIT_BRANCH#refs/tags/}"
 export S3_URL="s3://downloads.mesosphere.io/cassandra/assets/${VERSION}/cli/${PLATFORM}/dcos-cassandra"
