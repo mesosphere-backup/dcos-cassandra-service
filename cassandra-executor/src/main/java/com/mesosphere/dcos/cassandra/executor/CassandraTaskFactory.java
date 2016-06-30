b8221b871890e52e034f57c006d8f0ac25a600af
Gabriel Hartmann <gabriel@mesosphere.io>
2016-06-29 22:43:56 -0700

    Add UlimitExecutorTask

--- cassandra-executor/src/main/java/com/mesosphere/dcos/cassandra/executor/CassandraTaskFactory.java
+++ cassandra-executor/src/main/java/com/mesosphere/dcos/cassandra/executor/CassandraTaskFactory.java
@@ -1,5 +1,6 @@
 package com.mesosphere.dcos.cassandra.executor;
 
+import com.mesosphere.dcos.cassandra.executor.tasks.UlimitExecutorTask;
 import org.apache.mesos.ExecutorDriver;
 import org.apache.mesos.Protos;
 import org.apache.mesos.executor.ExecutorTask;
@@ -9,6 +10,7 @@ import org.apache.mesos.executor.TimedExecutorTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -25,6 +27,32 @@ public class CassandraTaskFactory implements org.apache.mesos.executor.ExecutorT
     @Override
     public List<TimedExecutorTask> createTimedTasks(String taskType, Protos.ExecutorInfo executorInfo, ExecutorDriver driver) throws ExecutorTaskException {
         LOGGER.info("Received request to createTimedTasks with ExecutorInfo:" + executorInfo);
-        return null;
+        return Arrays.asList(
+            new UlimitExecutorTask("-l", getMemlock(executorInfo)), // The maximum size that can be locked into memory.
+            new UlimitExecutorTask("-n", getNofile(executorInfo)),  // The maximum number of open file descriptors.
+            new UlimitExecutorTask("-u", getNproc(executorInfo)));  // The maximum number of processes available to a single user.
+    }
+
+    private String getMemlock(Protos.ExecutorInfo executorInfo) {
+        return getEnvironmentValue(executorInfo,"CASSANDRA_ULIMIT_MEMLOCK");
+    }
+
+    private String getNofile(Protos.ExecutorInfo executorInfo) {
+        return getEnvironmentValue(executorInfo,"CASSANDRA_ULIMIT_NOFILE");
+    }
+
+    private String getNproc(Protos.ExecutorInfo executorInfo) {
+        return getEnvironmentValue(executorInfo,"CASSANDRA_ULIMIT_NPROC");
+    }
+
+    private String getEnvironmentValue(Protos.ExecutorInfo executorInfo, String key) {
+        for (Protos.Environment.Variable var : executorInfo.getCommand().getEnvironment().getVariablesList()) {
+            if (key.equals(var.getName())) {
+               return var.getValue();
+            }
+        }
+
+        // An empty string as an argument to 'ulimit' has the effect of merely printing the ulimit value.
+        return "";
     }
 }
