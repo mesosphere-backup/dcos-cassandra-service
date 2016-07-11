272b2b09482599a77f00ed5a7d276378bf48d2b9
Gabriel Hartmann <gabriel@mesosphere.io>
2016-07-11 11:28:32 -0700

    Remove log redirection.  All logs should go to stdout/stderr

--- cassandra-executor/src/main/java/com/mesosphere/dcos/cassandra/executor/CassandraDaemonProcess.java
+++ cassandra-executor/src/main/java/com/mesosphere/dcos/cassandra/executor/CassandraDaemonProcess.java
@@ -271,14 +271,11 @@ public class CassandraDaemonProcess {
     private Process createDaemon() throws IOException {
 
         final ProcessBuilder builder = new ProcessBuilder(
-            paths.cassandraRun()
-                .toString(),
+            paths.cassandraRun().toString(),
             getReplaceIp(),
-            "-f"
-        )
-            .directory(new File(System.getProperty("user.dir")))
-            .redirectOutput(new File("cassandra-stdout.log"))
-            .redirectError(new File("cassandra-stderr.log"));
+            "-f")
+            .inheritIO()
+            .directory(new File(System.getProperty("user.dir")));
 
         builder.environment().putAll(task.getConfig().getHeap().toEnv());
         builder.environment().put(
