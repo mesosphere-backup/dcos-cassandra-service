d2e204feb2174189dad4443092ab9593e63e2537
Nicholas Parker <nick@nickbp.com>
2016-09-09 09:48:07 -0700

    Add 'test.sh' test runner script (#236)
    * Add 'test.sh' test runner script
    
    test.sh invokes 'run_tests.py' in dcos-commons/tools/ for common bootstrap handling
    
    * Fix test script's fetch of commons tools
    
    * Fix private agent setting
    
    * Fix cluster launching, resync to new run_tests.py args
    
    * Fix agent count passthrough to launcher

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Exit immediately on errors -- the helper scripts all emit github statuses internally
+set -e
+
+REPO_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $REPO_ROOT_DIR
+
+# Grab dcos-commons build/release tools:
+rm -rf dcos-commons-tools/ && curl https://infinity-artifacts.s3.amazonaws.com/dcos-commons-tools.tgz | tar xz
+
+# Get a CCM cluster if not already configured (see available settings in dcos-commons/tools/README.md):
+if [ -z "$CLUSTER_URL" ]; then
+    echo "CLUSTER_URL is empty/unset, launching new cluster."
+    export CCM_AGENTS=5
+    CLUSTER_ID_URL=$(./dcos-commons-tools/launch_ccm_cluster.py)
+    export CLUSTER_URL=$(echo "${CLUSTER_ID_URL}" | jq .url)
+    export CLUSTER_ID=$(echo "${CLUSTER_ID_URL}" | jq .id)
+else
+    echo "Using provided CLUSTER_URL as cluster: $CLUSTER_URL"
+fi
+
+# Run shakedown tests:
+${REPO_ROOT_DIR}/dcos-commons-tools/run_tests.py shakedown ${REPO_ROOT_DIR}/integration/tests/ ${REPO_ROOT_DIR}/integration/requirements.txt
+
+# Run legacy dcos-tests:
+if [ -d "${REPO_ROOT_DIR}/dcos-tests" ]; then
+    ${REPO_ROOT_DIR}/dcos-commons-tools/run_tests.py dcos-tests ${REPO_ROOT_DIR}/dcos-tests/infinitytests/kafka ${REPO_ROOT_DIR}/dcos-tests/
+else
+    echo "${REPO_ROOT_DIR}/dcos-tests/ not found, skipping dcos-tests"
+fi
