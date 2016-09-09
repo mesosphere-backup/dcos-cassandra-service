08c223c736e54b2ed8195c4fac663ed0ee25adc3
Nick <nick@mesosphere.io>
2016-09-08 17:31:28 -0700

    Fix cluster launching, resync to new run_tests.py args

--- test.sh
+++ test.sh
@@ -12,20 +12,20 @@ rm -rf dcos-commons-tools/ && curl https://infinity-artifacts.s3.amazonaws.com/d
 # Get a CCM cluster if not already configured (see available settings in dcos-commons/tools/README.md):
 if [ -z "$CLUSTER_URL" ]; then
     echo "CLUSTER_URL is empty/unset, launching new cluster."
-    export CCM_AGENTS=5
-    CLUSTER_URL=$(./dcos-commons-tools/launch_ccm_cluster.py)
-    echo $?
-    export CLUSTER_URL=$CLUSTER_URL
+    CCM_AGENTS=5
+    CLUSTER_ID_URL=$(./dcos-commons-tools/launch_ccm_cluster.py)
+    export CLUSTER_URL=$(echo "${CLUSTER_ID_URL}" | jq .url)
+    export CLUSTER_ID=$(echo "${CLUSTER_ID_URL}" | jq .id)
 else
     echo "Using provided CLUSTER_URL as cluster: $CLUSTER_URL"
 fi
 
 # Run shakedown tests:
-${REPO_ROOT_DIR}/dcos-commons-tools/run_tests.py shakedown "$CLUSTER_URL" ${REPO_ROOT_DIR}/integration/tests/ ${REPO_ROOT_DIR}/integration/requirements.txt
+${REPO_ROOT_DIR}/dcos-commons-tools/run_tests.py shakedown ${REPO_ROOT_DIR}/integration/tests/ ${REPO_ROOT_DIR}/integration/requirements.txt
 
 # Run legacy dcos-tests:
 if [ -d "${REPO_ROOT_DIR}/dcos-tests" ]; then
-    ${REPO_ROOT_DIR}/dcos-commons-tools/run_tests.py dcos-tests "$CLUSTER_URL" ${REPO_ROOT_DIR}/dcos-tests/infinitytests/kafka ${REPO_ROOT_DIR}/dcos-tests/
+    ${REPO_ROOT_DIR}/dcos-commons-tools/run_tests.py dcos-tests ${REPO_ROOT_DIR}/dcos-tests/infinitytests/kafka ${REPO_ROOT_DIR}/dcos-tests/
 else
     echo "${REPO_ROOT_DIR}/dcos-tests/ not found, skipping dcos-tests"
 fi
