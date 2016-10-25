2c365911b9e42d88f062449feda69d0b2bdb616e
Michael Browning <mrbrowning@distopia.net>
2016-10-25 16:11:45 -0700

    Run janitor with correct principal and master url when in strict mode (#297)

--- integration/tests/defaults.py
+++ integration/tests/defaults.py
@@ -8,3 +8,6 @@ TASK_RUNNING_STATE = 'TASK_RUNNING'
 
 DCOS_URL = shakedown.run_dcos_command('config show core.dcos_url')[0].strip()
 OPTIONS_FILE = os.environ.get('OPTIONS_FILE')
+
+IS_STRICT = bool(os.environ.get('STRICT_MODE', 'False'))
+PRINCIPAL = os.environ.get('FRAMEWORK_PRINCIPAL', 'cassandra-principal')
