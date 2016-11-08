e943c0a0aaff526df7d965ee8a5ab0f040373cf8
Nicholas Parker <nick@nickbp.com>
2016-11-07 17:20:40 -0800

    Speed up tests by only re-installing when needed, finish strict mode support (#309)
    * Speed up tests by only re-installing when needed, finish strict mode support
    
    - Just install once at the start of the suite, only reinstall if needed
    - Better logging in spin()
    - Streamlined handling of strict mode snowflakes
    
    * Less verbose janitoring
    
    * Move permission setup into test.sh
    
    * Don't modify input dict

--- integration/tests/defaults.py
+++ integration/tests/defaults.py
@@ -7,7 +7,19 @@ PACKAGE_NAME = 'cassandra'
 TASK_RUNNING_STATE = 'TASK_RUNNING'
 
 DCOS_URL = shakedown.run_dcos_command('config show core.dcos_url')[0].strip()
-OPTIONS_FILE = os.environ.get('OPTIONS_FILE')
 
-IS_STRICT = bool(os.environ.get('STRICT_MODE', 'False'))
-PRINCIPAL = os.environ.get('FRAMEWORK_PRINCIPAL', 'cassandra-principal')
+# expected SECURITY values: 'permissive', 'strict', 'disabled'
+if os.environ.get('SECURITY', '') == 'strict':
+    print('Using strict mode test configuration')
+    PRINCIPAL = 'service-acct'
+    DEFAULT_OPTIONS_DICT = {
+        "service": {
+            "user": "nobody",
+            "principal": PRINCIPAL,
+            "secret_name": "secret"
+        }
+    }
+else:
+    print('Using default test configuration')
+    PRINCIPAL = 'cassandra-principal'
+    DEFAULT_OPTIONS_DICT = {}
