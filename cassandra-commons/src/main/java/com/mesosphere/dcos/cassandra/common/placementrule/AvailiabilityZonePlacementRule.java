cd251cd149af5325041c64376c14afd1ed9736b6
Arun Kumar <arukum@adobe.com>
2017-08-03 20:35:55 +0530

    move az login to worker

--- cassandra-commons/src/main/java/com/mesosphere/dcos/cassandra/common/placementrule/AvailiabilityZonePlacementRule.java
+++ cassandra-commons/src/main/java/com/mesosphere/dcos/cassandra/common/placementrule/AvailiabilityZonePlacementRule.java
@@ -3,38 +3,70 @@ package com.mesosphere.dcos.cassandra.common.placementrule;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.mesos.Protos.Attribute;
 import org.apache.mesos.Protos.Offer;
 import org.apache.mesos.Protos.TaskInfo;
+import org.apache.mesos.config.ConfigStoreException;
 import org.apache.mesos.offer.OfferRequirement;
 import org.apache.mesos.offer.constrain.PlacementRule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mesosphere.dcos.cassandra.common.config.CassandraSchedulerConfiguration;
+import com.mesosphere.dcos.cassandra.common.config.DefaultConfigurationManager;
 
 public class AvailiabilityZonePlacementRule implements PlacementRule {
 
-	private String availiabilityZone;
-	private final static String AVAILIABLITY_ZONE_ATTRIBUTE_NAME = "zone";
+    private static final Logger LOGGER = LoggerFactory.getLogger(AvailiabilityZonePlacementRule.class);
+    private final static String AVAILIABLITY_ZONE_ATTRIBUTE_NAME = "zone";
+    private DefaultConfigurationManager configurationManager;
 
-	public AvailiabilityZonePlacementRule(String availiabilityZone) {
-		this.availiabilityZone = availiabilityZone;
-	}
+    public AvailiabilityZonePlacementRule(DefaultConfigurationManager configurationManager) {
+        this.configurationManager = configurationManager;
+    }
 
-	@Override
-	public Offer filter(Offer offer, OfferRequirement offerRequirement, Collection<TaskInfo> tasks) {
-		List<Attribute> attributes = offer.getAttributesList();
-		for (Attribute attribute : attributes) {
-			if (AVAILIABLITY_ZONE_ATTRIBUTE_NAME.equals(attribute.getName())) {
-				if (attribute.getText().getValue().equals(availiabilityZone)) {
-					return offer;
-				}
-			}
-		}
-		return offer.toBuilder().clearResources().build();
-	}
+    @Override
+    public Offer filter(Offer offer, OfferRequirement offerRequirement, Collection<TaskInfo> tasks) {
+        CassandraSchedulerConfiguration schedulerConfiguration;
+        List<Attribute> attributes = offer.getAttributesList();
+        String hostname = offer.getHostname();
+        boolean azKnown = false;
+        try {
+            schedulerConfiguration = (CassandraSchedulerConfiguration) configurationManager.getTargetConfig();
+        } catch (ConfigStoreException e) {
+            LOGGER.error("Scheduler config manager read failed, will reject this offer:" + e.getMessage(), e);
+            return offer.toBuilder().clearResources().build();
+        }
+        LOGGER.info("AvailiabilityZonePlacementRule filter with host: {}", hostname);
+        for (Attribute attribute : attributes) {
+            if (AVAILIABLITY_ZONE_ATTRIBUTE_NAME.equals(attribute.getName())) {
+                azKnown = true;
+                String zones = schedulerConfiguration.getZones();
+                if(isValidOffer(zones,hostname,attribute.getText().getValue())) {
+                    return offer;
+                }
+            }
+        }
+        if(!azKnown){
+            return offer;
+        }
+        return offer.toBuilder().clearResources().build();
+    }
 
-	@Override
-	public String toString() {
-		return "AvailiabilityZonePlacementRule [availiabilityZone=" + availiabilityZone + "]";
-	}
-	
-	
+    private boolean isValidOffer(String zones, String hostname, String zoneName) {
+        //Its in formatter node-0:AZ:hostip,node-1:AZ:hostIp
+        String[] azHostMap = zones.split(",");
+        for(String text :azHostMap) {
+            if(text.contains(hostname) && text.contains(zoneName)) {
+                //Matches can be validation with split with ':' then match with [0] and [1]
+                //text.split(":"); and then can be matched with exact string
+                LOGGER.info("Found match for az :"+text);
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
 }
